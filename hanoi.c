433f5522207b42b9de029be3f8a09278b8113b81
Matej <dobrakmato@gmail.com>
2019-09-30 20:04:21 +0200

    omylom som vymazal main ked som prekladal

--- hanoi.c
+++ hanoi.c
@@ -3,19 +3,13 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("posun kamen 1 z %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("posun kamen  %d z %d na %d\n", n, a, b);
     hanoi(n, 6 - a - b, b);
   }
 }
 
-int main()
-{
-  int n = 4;
-
-  hanoi(n, 1, 2);
-  return 0;
-}
+# omylom som vymazal main
